8124233a678510c1b5740e86dffa8f3a5fc24898
Rohan Ray <69780075+roray02@users.noreply.github.com>
2024-11-10 18:58:30 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PointsCounter from './components/PointsCounter'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <PointsCounter />
+    </div>
+  )
+}
+
+export default App
